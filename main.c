bc01aa407be8eeb5a046ee9a5fb9f7e5b3944c21
Omar Ahmed <132203684+Omar-Ahmed-Aboziada@users.noreply.github.com>
2023-11-19 14:33:15 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Employee
+{
+    int ID;
+    char Name[20];
+    int Salary;
+};
+
+typedef struct Employee Employee;
+
+int main()
+{
+    /*Employee *emps[100];
+
+    emps[0] = malloc(1 * sizeof(Employee));
+    emps[1] = malloc(1 * sizeof(Employee));*/
+
+
+
+
+    return 0;
+}
